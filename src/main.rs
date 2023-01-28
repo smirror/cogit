902a507df06401914c3209766eecf266faf72449
libra <23025996+smirror@users.noreply.github.com>
2023-01-28 14:04:39 +0900

    read line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+extern crate exitcode;
+
+use std::env;
+use std::ops::Index;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    let args:Vec<String> = env::args().collect();
+    if args.len() == 1{
+        // TODO: add help messages
+        println!("plz subcommand.");
+        exit(exitcode::OK)
+    }
+
+    match args.index(1).as_str() {
+        "add" => {
+            println!("add")
+        }
+        "cat-file" => {
+            println!("cat-file")
+        }
+        "commit" => {
+            println!("init")
+        }
+        "diff" => {
+            println!("diff")
+        }
+        "init" => {
+            println!("init")
+        }
+        "status" => {
+            println!("status")
+        }
+        "log" => {
+            println!("log")
+        }
+        _ => {
+            println!("sorry. not {} subcommand.", args.index(1));
+        }
+    }
 }
