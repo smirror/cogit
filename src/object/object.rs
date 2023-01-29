6c5c65e50d6bdc6163c356d09ea083fdfb0646b3
libra <23025996+smirror@users.noreply.github.com>
2023-01-29 21:35:02 +0900

    change file

--- src/object/object.rs
+++ src/object/object.rs
@@ -1,10 +1,18 @@
 use sha1::{Sha1, Digest};
+use crate::object::object_type;
 
-struct Object {
+#[derive(Debug, Clone)]
+pub struct Object {
     Hash: Sha1,
     Type: Type,
     Size: i8,
     Data: [byte],
+}
+
+impl fmt::Display for Object {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Hash:{}, Type:{}, Size:{}, Data", self.Hash, self.Type, self.Size, self.Data)
+    }
 }
 
 impl Object {
